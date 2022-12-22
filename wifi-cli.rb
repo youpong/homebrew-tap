d267bd69e03bcef4daff39cc2b8c93b4da6faa34
Tanmay Patil <92677342+TanmayPatil105@users.noreply.github.com>
2022-12-22 13:19:59 +0530

    Added LICENSE

--- wifi-cli.rb
+++ wifi-cli.rb
@@ -3,9 +3,8 @@ class WifiCli < Formula
     homepage ""
     url "https://github.com/TanmayPatil105/wifi-cli/archive/refs/tags/v0.0.1.tar.gz"
     sha256 "cfcd367b9d6612a1fbb5e26285283386011699ee688d4abbbb22bb0d317321bb"
-    license ""
+    license "GPL-3.0"
   
-    # depends_on "cmake" => :build
   
     def install
       system "./configure"
