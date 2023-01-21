baef8910e6d6a0bdc55f2e552b2ab5d3b2f038af
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-01-21 23:45:30 +0530

    🔧 Update Description

--- reduce.rb
+++ reduce.rb
@@ -1,5 +1,5 @@
 class Reduce < Formula
-    desc "A command line URL shortener using reduced.to API"
+    desc "⚡ Extremely fast Command line URL shortener using reduced.to API"
     homepage ""
     url "https://github.com/TanmayPatil105/reduce/archive/refs/tags/v0.0.5.tar.gz"
     sha256 "3e159af0c04b690d1dc7d6a8de34c74799cd5c7bcf8840eba05587a0570e8998"
--- release.sh
+++ release.sh
@@ -2,13 +2,14 @@ GITHUB_NAME="TanmayPatil105"
 GITHUB_REPO="reduce"
 TAP_NAME="reduce"
 BINARY_NAME="reduce"
+DESCRIPTION=$(curl -s -L https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO | jq -r  .description)
 LICENSE=$(curl -s -L https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO | jq -r  .license.spdx_id)
 VERSION=$(curl -s https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO/releases/latest | jq -r .tag_name)
 SHA256=$(curl -s -L "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz" | shasum -a 256 | cut -d " " -f 1)
 
 cat > $TAP_NAME.rb <<EOF
 class Reduce < Formula
-    desc "A command line URL shortener using reduced.to API"
+    desc "$DESCRIPTION"
     homepage ""
     url "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz"
     sha256 "$SHA256"
