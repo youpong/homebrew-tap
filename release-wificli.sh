a51b39f3dd22c14fd43a9930324bc6fa185714a6
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-01-27 14:56:03 +0530

    🚀 Added wifi-cli release script

---
+++ release-wificli.sh
@@ -0,0 +1,29 @@
+GITHUB_NAME="TanmayPatil105"
+GITHUB_REPO="wifi-cli"
+TAP_NAME="wifi-cli"
+BINARY_NAME="wifi"
+REPO=$(curl -s -L https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO)
+DESCRIPTION=$(echo $REPO | jq -r  .description)
+LICENSE=$(echo $REPO | jq -r  .license.spdx_id)
+VERSION=$(curl -s https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO/releases/latest | jq -r .tag_name)
+SHA256=$(curl -s -L "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz" | shasum -a 256 | cut -d " " -f 1)
+
+cat > $TAP_NAME.rb <<EOF
+class WifiCli < Formula
+    desc "$DESCRIPTION"
+    homepage "https://github.com/$GITHUB_NAME/$GITHUB_REPO"
+    url "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "$SHA256"
+    license "$LICENSE"
+  
+  
+    def install
+      system "./configure"
+      bin.install "src/$BINARY_NAME"
+    end
+  
+    test do
+      system "#{bin}/$BINARY_NAME -h"
+    end
+  end
+EOF
--- wifi-cli.rb
+++ wifi-cli.rb
@@ -1,5 +1,5 @@
 class WifiCli < Formula
-    desc "Wi-fi through command line"
+    desc "📶 Wifi through command line"
     homepage "https://github.com/TanmayPatil105/wifi-cli"
     url "https://github.com/TanmayPatil105/wifi-cli/archive/refs/tags/v0.0.1.tar.gz"
     sha256 "cfcd367b9d6612a1fbb5e26285283386011699ee688d4abbbb22bb0d317321bb"
