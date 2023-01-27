b2cd1fd8793d7bef5851c12b5cdd294e3b1d92a7
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-01-27 11:40:16 +0530

    🚀 Added procfetch release script

--- procfetch.rb
+++ procfetch.rb
@@ -1,12 +1,10 @@
 class Procfetch < Formula
-    desc "A command-line system information utility written in C++"
+    desc "🎨 A command-line system information utility written in C++"
     homepage "https://tanmaypatil105.github.io/procfetch/"
-    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.0.2.tar.gz"
-    sha256 "073f02c6823b5597580e528029d3f2b0e071cad483c6576f1437c7c1072d40cd"
+    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.0.3.tar.gz"
+    sha256 "4900b77a4a2210d9c07eb21ee6727cb4e5ce5fdcfc143e6781847a3b30816693"
     license "GPL-3.0"
-  
-    # depends_on "cmake" => :build
-  
+    
     def install
       system "make"
       system "sudo mkdir -p /usr/share/procfetch/ascii"
---
+++ release-procfetch.sh
@@ -0,0 +1,30 @@
+GITHUB_NAME="TanmayPatil105"
+GITHUB_REPO="procfetch"
+TAP_NAME="procfetch"
+BINARY_NAME="procfetch"
+REPO=$(curl -s -L https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO)
+DESCRIPTION=$(echo $REPO | jq -r  .description)
+LICENSE=$(echo $REPO | jq -r  .license.spdx_id)
+VERSION=$(curl -s https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPO/releases/latest | jq -r .tag_name)
+SHA256=$(curl -s -L "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz" | shasum -a 256 | cut -d " " -f 1)
+
+cat > $TAP_NAME.rb <<EOF
+class Procfetch < Formula
+    desc "$DESCRIPTION"
+    homepage "https://tanmaypatil105.github.io/procfetch/"
+    url "https://github.com/$GITHUB_NAME/$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz"
+    sha256 "$SHA256"
+    license "$LICENSE"
+    
+    def install
+      system "make"
+      system "sudo mkdir -p /usr/share/procfetch/ascii"
+      system "sudo cp ascii/* /usr/share/procfetch/ascii/"
+      bin.install "src/$BINARY_NAME"
+    end
+  
+    test do
+      system "$BINARY_NAME -t"
+    end
+  end
+EOF
