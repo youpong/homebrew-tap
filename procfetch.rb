ec2ed393f9d6368ffe484ef7016c17ee01268e78
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-01-28 21:45:17 +0530

    Revert "🚀 Update Homebrew Taps"
    This reverts commit eab90bf4d47d46f2f0f00aad6e941eb7a085e6bd.

--- procfetch.rb
+++ procfetch.rb
@@ -1,9 +1,9 @@
 class Procfetch < Formula
-    desc "null"
+    desc "🎨 A command-line system information utility written in C++"
     homepage "https://tanmaypatil105.github.io/procfetch/"
-    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/null.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
-    license "null"
+    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.0.3.tar.gz"
+    sha256 "4900b77a4a2210d9c07eb21ee6727cb4e5ce5fdcfc143e6781847a3b30816693"
+    license "GPL-3.0"
     
     def install
       system "make"
