b732abc4943b7bf6fdbd3d1df4a23728654f7260
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-11 09:01:21 +0530

    📝 Update wifi-cli

--- wifi-cli.rb
+++ wifi-cli.rb
@@ -1,8 +1,8 @@
 class WifiCli < Formula
     desc "📶 Wifi through command line"
     homepage "https://github.com/TanmayPatil105/wifi-cli"
-    url "https://github.com/TanmayPatil105/wifi-cli/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "cfcd367b9d6612a1fbb5e26285283386011699ee688d4abbbb22bb0d317321bb"
+    url "https://github.com/TanmayPatil105/wifi-cli/archive/refs/tags/v0.0.2.tar.gz"
+    sha256 "3a63c49ac9bf4e5bb4df7cf40d6061ca274016dd6776246993301fdada18a2cb"
     license "GPL-3.0"
   
   
