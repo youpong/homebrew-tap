bfc0850cb599fdba8efb8be3d11e67ecfb8bef64
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-11 09:08:59 +0530

    📝 Update Taps

--- wifi-cli.rb
+++ wifi-cli.rb
@@ -1,9 +1,10 @@
 class WifiCli < Formula
-    desc "null"
+    desc "📶 Wifi through command line"
     homepage "https://github.com/TanmayPatil105/wifi-cli"
     url "https://github.com/TanmayPatil105/wifi-cli/archive/refs/tags/v0.0.2.tar.gz"
     sha256 "3a63c49ac9bf4e5bb4df7cf40d6061ca274016dd6776246993301fdada18a2cb"
     license "GPL-3.0"
+  
   
     def install
       system "./configure"
