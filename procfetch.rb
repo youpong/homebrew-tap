d16eb833a5133df24ae57ad5de6610427f68cedd
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-12 18:46:26 +0530

    🚀 Update Taps

--- procfetch.rb
+++ procfetch.rb
@@ -1,8 +1,8 @@
 class Procfetch < Formula
     desc "🎨 A command-line system information utility written in C++"
     homepage "https://tanmaypatil105.github.io/procfetch/"
-    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.0.3.tar.gz"
-    sha256 "4900b77a4a2210d9c07eb21ee6727cb4e5ce5fdcfc143e6781847a3b30816693"
+    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "86e2ed8f35b71d63123a4dcc1934190ccbf64457edc0d5bafa56193fda23464c"
     license "GPL-3.0"
     
     def install
