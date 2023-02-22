6d0d88db1cd80fb940cf35313a0088ebe68b6de3
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-22 19:59:54 +0530

    🚀 Update taps

--- procfetch.rb
+++ procfetch.rb
@@ -1,15 +1,15 @@
 class Procfetch < Formula
     desc "🎨 A command-line system information utility written in C++"
     homepage "https://tanmaypatil105.github.io/procfetch/"
-    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.1.5.tar.gz"
-    sha256 "88894f2610487e96300ea8ac6d31d9a3106e0628dbf39d7d49e32269ea699f95"
+    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "8c8721a51440d70bb30b031f2a55ee27a82494e71de5c8d9be70ab586c1b9ed5"
     license "GPL-3.0"
     
     def install
-      system "./configure"
+      system "./configure -C brew"
       system "make"
-      system "sudo mkdir -p /usr/share/procfetch/ascii"
-      system "sudo cp ascii/* /usr/share/procfetch/ascii/"
+      system "mkdir -p /home/linuxbrew/.linuxbrew/share/procfetch/ascii"
+      system "cp ascii/* /home/linuxbrew/.linuxbrew/share/procfetch/ascii"
       bin.install "src/procfetch"
     end
   
