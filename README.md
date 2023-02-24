718db8efb33f23a14df0fd85a6936cfcfb73f37b
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-24 22:44:24 +0530

    🚀 Update taps

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 | :---     |   :---: | :---: |---:
 | reduce | Command line URL shortener using reduced.to API | [reduce](https://github.com/TanmayPatil105/reduce) | v0.0.5
 | wifi-cli   | Wi-fi through command line | [wifi](https://github.com/TanmayPatil105/wifi-cli) | v0.0.2
-| procfetch  | A command-line system information utility written in C++ | [procfetch](https://github.com/TanmayPatil105/procfetch) | v0.2.0
+| procfetch  | A command-line system information utility written in C++ | [procfetch](https://github.com/TanmayPatil105/procfetch) | v0.2.1
 
 
 ### Tappping
--- procfetch.rb
+++ procfetch.rb
@@ -1,10 +1,10 @@
 class Procfetch < Formula
     desc "🎨 A command-line system information utility written in C++"
     homepage "https://tanmaypatil105.github.io/procfetch/"
-    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.2.0.tar.gz"
-    sha256 "8c8721a51440d70bb30b031f2a55ee27a82494e71de5c8d9be70ab586c1b9ed5"
+    url "https://github.com/TanmayPatil105/procfetch/archive/refs/tags/v0.2.1.tar.gz"
+    sha256 "8a67ff24f974ddea52de268b593a1ef8c825160bf941af01de4942fb043d2fe2"
     license "GPL-3.0"
-  
+    
     def install
       system "./configure", "--prefix=#{prefix}"
       system "make", "install"
@@ -13,4 +13,4 @@ class Procfetch < Formula
     test do
       system "procfetch -t"
     end
-end
+  end
