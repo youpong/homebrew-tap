c4b3aa8c506a273725e3e101ddd648a7e3f2f393
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-02-24 22:57:37 +0530

    ⏪ Revert changes temporarily

--- procfetch.rb
+++ procfetch.rb
@@ -6,8 +6,11 @@ class Procfetch < Formula
     license "GPL-3.0"
     
     def install
-      system "./configure", "--prefix=#{prefix}"
-      system "make", "install"
+      system "./configure"
+      system "make"
+      system "sudo mkdir -p /usr/share/procfetch/ascii"
+      system "sudo cp ascii/* /usr/share/procfetch/ascii/"
+      bin.install "src/procfetch"
     end
   
     test do
